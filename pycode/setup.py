7be23cae68fbbe9471e202d6519b73a87413760b
Abu Rah <82013248d@gmail.com>
2014-03-20 20:34:59 -0500

    Initialization Commit take-2

---
+++ pycode/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except Importerror
+	from distutils.core import setup
+
+config = {
+		'description':'A simple text editor written in python; using pyside for GUI',
+		'author' : 'Abu_Rah',
+		'url' : 'URL to get it at.',
+		'download_url':'where to download at.',
+		'author_email':'82013248d@gmail.com',
+		'version':'0.1',
+		'install_requires':['distribute', "pyside"],
+		'packages':['pycode'],
+		'scripts':[],
+		'name':'projectname'
+}
+
+setup(**config)
---
+++ pycodesetup.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+# Intial set-up for PyCode development
+
+sudo apt-get cmake
+sudo aptitude install qt4-qmake qt4-dev-tools python-qt4 python-qt4-dev
